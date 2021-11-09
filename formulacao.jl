10f8f20b082e0aeb70d71b734e4b81514aac8595
leorpedroso <leorpedroso97@gmail.com>
2021-11-09 00:20:08 -0300

    arquivo .jl

---
+++ formulacao.jl
@@ -0,0 +1,84 @@
+using JuMP
+using GLPK
+
+print("abriu")
+m = Model();
+set_optimizer(m, GLPK.Optimizer);
+
+set_time_limit_sec(m, 1800.0);
+
+function reader(filepath :: String)
+  k_count = 0
+  arestas = []
+  v_count = 0
+  
+  open(filepath, "r") do f
+    line = readline(f)
+    line = readline(f)
+    v_count = parse(Int, line) # Number of vertices
+    line = readline(f)
+    line = readline(f)
+
+    while ! eof(f)
+      line = readline(f)
+      words = split(line)
+      if isempty(words)
+          continue
+      end
+      vertex1 = parse(Int,words[1])
+      vertex2 = parse(Int,words[2])
+      edge_type = parse(Int,words[3])
+
+      if edge_type > k_count
+        k_count = edge_type
+      end
+
+      push!(arestas, (vertex1,vertex2,edge_type))
+    end
+  end
+  return (arestas, k_count, v_count)
+end
+
+filepath = ARGS[1]
+print(filepath)
+my_tuple = reader(filepath)
+arestas = my_tuple[1]
+K = my_tuple[2]
+V = my_tuple[3];
+
+function create_arestas_k(arestas, k)
+    arestas_k = [[] for i=1:k]
+    for a in arestas
+        push!(arestas_k[a[3]], (a))
+    end
+    return arestas_k
+end
+
+arestas_k = create_arestas_k(arestas,K);
+
+@variable(m, x[i in arestas], Bin);
+
+@constraint(m, [k in 1:K], sum(x[i] for i in arestas_k[k]) <=1);
+
+function create_arestas_v(arestas, v)
+    arestas_v = [[] for i=1:v]
+    for a in arestas
+        push!(arestas_v[a[1]], (a))
+        push!(arestas_v[a[2]], (a))
+    end
+    return arestas_v
+end
+
+arestas_v = create_arestas_v(arestas,V);
+
+@constraint(m, [v in 1:V], sum(x[i] for i in arestas_v[v]) <=1);
+
+@objective(m, Max, sum(x[i] for i in arestas));
+
+optimize!(m)
+
+print(objective_value(m))
+
+@show value.(x)
+
+
